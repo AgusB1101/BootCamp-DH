caae6939bcd62be301978c4fd0019bfebc4b46e7
AgusB1101 <agustinbernal69@gmail.com>
2021-11-02 17:39:54 -0300

    Flexbox example

--- HTML/Test/app.js
+++ HTML/Test/app.js
@@ -1,9 +1,14 @@
-import express from "express";
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 3000 || process.env;
 
 console.clear();
 
-app.get("/", (_, res) => res.send("Hello World!"));
+app.use(express.static("public"));
+
+app.get("/", (_, res) =>
+  res.sendFile(path.join(__dirname, "./views/home.html"))
+);
 
 app.listen(PORT, () => console.log("Running server in port " + PORT));
