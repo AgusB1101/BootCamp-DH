a31add1ca84686a03af0df74c3813acfb1eeb3fa
AgusB1101 <agustinbernal69@gmail.com>
2021-11-11 16:25:48 -0300

    Separated files and added register to main

--- Express/app.js
+++ Express/app.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const app = express();
+const session = require("express-session");
 const mainRoutes = require("./src/routes/mainRoutes");
-const registerRoutes = require("./src/routes/registerRoutes");
+
+const app = express();
 
 console.clear();
 
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  session({ secret: "This is a secret", resave: true, saveUninitialized: true })
+);
 
 app.use("/", mainRoutes);
-app.use("/register", registerRoutes);
 
 app.listen(3000, () => console.log("Server listening on port 3000"));
